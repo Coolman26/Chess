14436b2a1e69419180508ae618bde0bd95cb4796
Coolman26 <jackgreenwald4@gmail.com>
2022-11-08 19:38:05 -0500

    --- Auto Git Commit ---

--- PythonFiles/promotion.py
+++ PythonFiles/promotion.py
@@ -20,14 +20,14 @@ def promotionBoard(vars, eventGet, mouseXY):
     newX = boardSize - alphabet.index(promotion[0]) - (3 if promotion[0] != "G" else 1)
     for y in range(2):
         for x in range(2):
-            
+            tileX = squareSize//2*(x+1) + squareSize//2*x + squareSize*newX
             xEven = x % 2 == 0
             yEven = y % 2 == 0
             if (xEven and yEven) or (not xEven and not yEven):
-                pygame.draw.rect(screen, tileColor2, [squareSize//2*(x+1) + squareSize//2*x + squareSize*newX, screenDems[1] - squareSize//2*(
+                pygame.draw.rect(screen, tileColor2, [tileX, screenDems[1] - squareSize//2*(
                     y+1) + squareSize//2*y - (y+1) * squareSize, squareSize, squareSize])
             else:
-                    pygame.draw.rect(screen, tileColor1, [squareSize//2*(x+1) + squareSize//2*x + squareSize*newX, screenDems[1] - squareSize//2*(
+                    pygame.draw.rect(screen, tileColor1, [tile, screenDems[1] - squareSize//2*(
                         y+1) + squareSize//2*y - (y+1)*squareSize, squareSize, squareSize])
             screen.blit(promotionPieces[promotion[2] + str(promotionPiece)], [squareSize//2*(x+1) + squareSize//2*x +
                             squareSize*newX + 10, screenDems[1] - squareSize//2*(y+1) + squareSize//2*y - (y+1)*squareSize + 10])
