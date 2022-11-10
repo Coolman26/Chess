0edaf1d638a618697f2b6a9e414466cd5b7fea0e
Coolman26 <jackgreenwald4@gmail.com>
2022-11-09 23:26:15 -0500

    --- Auto Git Commit ---

--- test.py
+++ test.py
@@ -6,7 +6,10 @@ im = im.convert('RGBA')
 
 data = np.array(im)   # "data" is a height x width x 4 numpy array
 red, green, blue, alpha = data.T # Temporarily unpack the bands for readability
-print(data.T)
+for i in range(len(red)):
+    for x in range(len(red[i])):
+        if red[i][x] == 255:
+            print(red[i][x], green[i][x], blue[i][x])
 
 # Replace white with red... (leaves alpha values alone...)
 white_areas = (red == 0) & (blue == 0) & (green == 0)
