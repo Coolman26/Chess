6819d60f1404a2a3d23c3215828275d0dae997fe
Coolman26 <jackgreenwald4@gmail.com>
2022-11-14 08:42:14 -0500

    --- Auto Git Commit ---

--- PythonFiles/editor.py
+++ PythonFiles/editor.py
@@ -3,11 +3,11 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
 
 import pygame
 pygame.font.init()
-font = pygame.font.SysFont('Comic Sans MS', 45)
+font = pygame.font.SysFont('Product Sans', 60)
  
 # create a text surface object,
 # on which text is drawn on it.
-text = font.render('Chess Editor', True, (28, 144, 147)) 
+text = font.render('Chess Editor', True, (222,202,175)) 
 # create a rectangular object for the
 # text surface object
 textRect = text.get_rect()
@@ -17,10 +17,10 @@ textRect = text.get_rect()
 
 def editor(vars):
     screen = vars["screen"]
-    screen.fill((89, 22, 160))
+    screen.fill((169,126,92))
     width = screen.get_width()
     height = screen.get_height()
-    textRect.center = (width // 2, 60)
+    textRect.center = (width // 2, 45)
     screen.blit(text, textRect)
 
     
